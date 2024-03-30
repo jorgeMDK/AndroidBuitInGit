1aab3c960bd5e9e1eca475ed39fb2b68bb83a02e
jorge <jorgemedelmdk@gmail.com>
2024-03-29 23:05:42 -0600

    modificaciones de Pantalla principal

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("hola")
-        print("quetal")
-        print("nuevamente")
+        val ButtonSaludar:Button = findViewById(R.id.button_saludar)
+        val ButtonDespedirse:Button = findViewById(R.id.button_despedirse)
+        val ButtonChocar:Button = findViewById(R.id.button_chocar)
+
+
     }
 }
